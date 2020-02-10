93a792b1e77e8ac3fadeccc1371a13ef14227156
ArtisanLRO <leorafaelorpilla@gmail.com>
2020-02-10 12:39:10 +1000

    Used malloc and pointers for game board, broke game down to helper functions, cleaned victory logic significantly, cleaned up comments

--- main.c
+++ main.c
@@ -9,7 +9,9 @@ by ArtisanLRO
 #include <stdlib.h>
 #include <stdbool.h>
 
-// For keyhandler logic
+#define BOARD_SIZE 3
+
+// Keyhandler logic.
 char getch(void)
 {
     char buf = 0;
@@ -32,191 +34,218 @@ char getch(void)
     return buf;
 }
 
-// Actual game logic -- all in one messy function
-int game()
-{
-    system("clear");
+// Set the current item in a given board position.
+void setGridItem(char* board, int x, int y, char item) {
+    *(board + y * BOARD_SIZE + x) = item; 
+}
 
-    const int BOARD_X_SIZE = 3;
-    const int BOARD_Y_SIZE = 3;
+// Show the current item in a given board position.
+char getGridItem(char* board, int x, int y) {
+    return *(board + y * BOARD_SIZE + x);
+}
 
-    const char PLAYER_1_CHARACTER = 'X';
-    const char PLAYER_2_CHARACTER = 'O';
-
-    // Game state variables
-    char currentPlayer = 'X';
-    char lastPlayer;
-
-    char ch;
-    char board[BOARD_X_SIZE][BOARD_Y_SIZE];
-    
+// Display the game board in the terminal.
+void renderBoard(char* board, int cursorX, int cursorY, bool cursorHidden) {
     int x, y;
-
-    int turnCount = 1;
     
-    int cursorX = 0;
-    int cursorY = 0;
+    for (y = 0; y < BOARD_SIZE; y++) {
+        for (x = 0; x < BOARD_SIZE; x++) {\
+            if (!cursorHidden && cursorX == x && cursorY == y) {
+                // If the player cursor is here, render the cursor.
+                printf("█ ");
+            }
+            
+            else {
+                // If not, show the contents according to pointer offset.
+                printf("%c ", getGridItem(board, x,y)); 
+            }
+            
+        }
+        printf("\n");
+    }
+}
 
-    bool illegalMove = false;
-    bool turnDone = false;
-
-    // Victory conditions
-    bool victoryVertical = false;
-    bool victoryHorizontal = false;
-    bool victoryDiagonal = false;
-
-    // For counting adjacent grids
-    int counter;
-    int emptySpace;
-
-    char current_character;
-    char checked_character;
-
-    for (y = 0; y < BOARD_Y_SIZE; y++) {
-        for (x = 0; x < BOARD_X_SIZE; x++) {
-            board[x][y] = '.';
+// Check for adjacent verticals.
+bool victoryVertical(char* board) {
+    int x, y;
+    
+    for (y = 0; y < BOARD_SIZE; y++) {
+        for (x = 0; x < BOARD_SIZE; x++) {
+            if (y > 0 && y != BOARD_SIZE-1) {
+                if (getGridItem(board, x, y) != '.' &&
+                        getGridItem(board, x, y) == getGridItem(board, x, y+1) && 
+                        getGridItem(board, x, y) == getGridItem(board, x, y-1)) {
+                    return 1;
+                }
+            }
         }
     }
 
-	while(1)
-	{
+    return 0;
+}
+
+// Check for adjacent horizontals.
+bool victoryHorizontal(char* board) {
+    int x, y;
+
+    for (y = 0; y < BOARD_SIZE; y++) {
+        for (x = 0; x < BOARD_SIZE; x++) {
+            if (x > 0 && x != BOARD_SIZE-1) {
+                if (getGridItem(board, x, y) != '.' &&
+                        getGridItem(board, x, y) == getGridItem(board, x+1, y) && 
+                        getGridItem(board, x, y) == getGridItem(board, x-1, y)) {
+                    return 1;
+                }
+            }
+        }
+    }
+    
+    return 0;
+}
+
+// Check for adjacent diagonals.
+bool victoryDiagonal(char* board) {
+    int x, y, counter;
+    char charBeingChecked;
+    
+    for (y = 0; y < BOARD_SIZE; y++) {
+        for (x = 0; x < BOARD_SIZE; x++) {
+                charBeingChecked = getGridItem(board, x, y);
+            if (charBeingChecked != '.' && 
+                    (x > 0 && x != BOARD_SIZE-1) && y > 0 && y != BOARD_SIZE-1) {
+                if (getGridItem(board, x, y) == getGridItem(board, x+1, y+1) && 
+                        getGridItem(board, x, y) == getGridItem(board, x-1, y-1)) {
+                    return 1;
+                }
+                if (getGridItem(board, x, y) == getGridItem(board, x+1, y-1) && 
+                        getGridItem(board, x, y) == getGridItem(board, x-1, y+1)) {
+                    return 1;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+
+bool victoryImpossible(char* board) {
+    int x, y;
+
+    for (y = 0; y < BOARD_SIZE; y++) {
+        for (x = 0; x < BOARD_SIZE; x++) {
+            if (getGridItem(board, x, y) == '.') {
+                return 0;
+            }
+        }
+    }
+
+    return 1;
+}
+
+// Prints any victory messages.
+bool victoryHandler(char* board, char lastPlayer) {
+    
+    if (victoryVertical(board)) {
+        printf("\nPlayer %c Victory: Vertical", lastPlayer);
+        printf("\nGame End");
+        printf("\n--------------------------------");
+        getch();
+        system("clear");
+        return 1;
+    }
+    else if (victoryHorizontal(board)) {
+        printf("\nPlayer %c Victory: Horizontal", lastPlayer);
+        printf("\nGame End");
+        printf("\n--------------------------------");
+        getch();
+        system("clear");
+        return 1;
+    }
+    else if (victoryDiagonal(board)) {
+        printf("\nPlayer %c Victory: Diagonal", lastPlayer);
+        printf("\nGame End");
+        printf("\n--------------------------------");
+        getch();
+        system("clear");
+        return 1;
+    }
+    else if (victoryImpossible(board)) {
+        printf("\nDraw: No Spaces Left");
+        printf("\nGame End");
+        printf("\n--------------------------------");
+        getch();
+        system("clear");
+        return 1;
+    }
+    return 0;
+}
+
+// Initialises game logic, then loops till victory or draw condition.
+int game() {
+
+    // Player symbols.
+    const char PLAYER_1_CHARACTER = 'X';
+    const char PLAYER_2_CHARACTER = 'O';
+
+    // Necessary to record the current player's turn.
+    char currentPlayer = 'X';
+    char lastPlayer;
+
+    // The game starts on the first turn.
+    int turnCount = 1;
+
+    // Initialise cursor position.
+    int cursorX, cursorY = 0;
+
+    // Status message helpers.
+    bool illegalMove, cursorHidden = false;
+
+    // Dynamically allocates memory for the square grid of the game board.
+    char *board = (char *)malloc(BOARD_SIZE * BOARD_SIZE * sizeof(char));
+
+    // Initialise the game board with blank space.
+    int x, y; 
+    for (y = 0; y < BOARD_SIZE; y++) {
+        for (x = 0; x < BOARD_SIZE; x++) {
+            setGridItem(board, x, y, '.');
+        }
+    }     
+
+    // The game loop.
+	while(1) {
+
+        // Clear the terminal.
         system("clear");
 
-        // Check for vertical adjacents -- at least 2
-        for (y = 0; y < BOARD_Y_SIZE; y++) {
-            checked_character = '.';
-            for (x = 0; x < BOARD_X_SIZE; x++) {
-                current_character = board[y][x];
-                if (current_character != checked_character) {
-                    counter = 1;
-                    checked_character = board[y][x];
-                }
-                else {
-                    counter++;
-                    if (current_character != '.' && counter == 3) {
-                        victoryVertical = true;
-                    }
-                }
-            }
-        }
-
-        // Check for horizontal adjacents -- at least 2
-        for (x = 0; x < BOARD_X_SIZE; x++) {
-            checked_character = '.';
-            for (y = 0; y < BOARD_Y_SIZE; y++) {
-                current_character = board[y][x];
-                if (current_character != checked_character) {
-                    counter = 1;
-                    checked_character = board[y][x];
-                }
-                else {
-                    counter++;
-                    if (current_character != '.' && counter == 3) {
-                        victoryHorizontal = true;
-                    }
-                }
-            }
-        }
-
-        // Check if no empty spaces left in the board for draw condition
-        emptySpace = 0;
-
-        for (y = 0; y < BOARD_Y_SIZE; y++) {
-            for (x = 0; x < BOARD_X_SIZE; x++) {
-                if (board[y][x] == '.') {
-                    emptySpace++;
-                }
-            }
-        }
-
-        // Prepare for the worst diagonal checking code to ever exist
-        for (y = 0; y < BOARD_Y_SIZE; y++) {
-            for (x = 0; x < BOARD_X_SIZE; x++) {
-                checked_character = board[y][x];
-                if (checked_character != '.' && ((x > 0 && x != BOARD_X_SIZE - 1) &&
-                        (y > 0 && y != BOARD_Y_SIZE - 1))) {
-                    if (board[y][x] == board[y+1][x+1] && 
-                        board[y][x] == board[y-1][x-1]) {
-                        victoryDiagonal = true;
-                    }
-                    else if (board[y][x] == board[y+1][x-1] && 
-                            board[y][x] == board[y-1][x+1]) {
-                        victoryDiagonal = true;
-                    }
-                }
-            }
-        }
-
+        // Print the header.
         printf("\nC99 Tic-Tac-Toe by ArtisanLRO");
         printf("\n--------------------------------\n");
-
-        // Prints the game board
-        for (y = 0; y < BOARD_Y_SIZE; y++) {
-            for (x = 0; x < BOARD_X_SIZE; x++) {
-                if (!turnDone && cursorX == x && cursorY == y) {
-                    // Print a cursor where it currently is
-                    printf("█ ");
-                }
-                else {
-                    // If not a cursor, print the grid value
-                    printf("%c ", board[x][y]);
-                }
-            }
-            printf("\n");
-        }
-
-        // Prints any victory messages
-        if (victoryVertical) {
-            printf("\nPlayer %c Victory: Vertical", lastPlayer);
-            printf("\nGame End");
-            printf("\n--------------------------------");
-            getch();
-            system("clear");
-            return 0;
-        }
-        else if (victoryHorizontal) {
-            printf("\nPlayer %c Victory: Horizontal", lastPlayer);
-            printf("\nGame End");
-            printf("\n--------------------------------");
-            getch();
-            system("clear");
-            return 0;
-        }
-        else if (victoryDiagonal) {
-            printf("\nPlayer %c Victory: Diagonal", lastPlayer);
-            printf("\nGame End");
-            printf("\n--------------------------------");
-            getch();
-            system("clear");
-            return 0;
-        }
-        else if (emptySpace == 0) {
-            printf("\nDraw: No Spaces Left");
-            printf("\nGame End");
-            printf("\n--------------------------------");
-            getch();
-            system("clear");
-            return 0;
-        }
-
-        // If no victory or draw conditions, turn swap
         
+        // Render the game board.
+        renderBoard(board, cursorX, cursorY, cursorHidden);
 
+        // Listen for game end conditions, end the game if any are true.
+        if (victoryHandler(board, lastPlayer)) {
+            return 1;
+        }
+
+        // Print game details.    
         printf("\n--------------------------------");
-        printf("\nTurn %d - Player %c", turnCount, currentPlayer);
-
-        // Turn off this message next time
+        printf("\nTurn %d - Player %c's Turn", turnCount, currentPlayer);
+        
         if (illegalMove) {
-            printf("\nMove Arrow Keys To Play - Turn Illegal");
+            printf("- Space Occupied");
             illegalMove = false;
         }
-        else {
-            printf("\nMove Arrow Keys To Play");
-        }
+
+        printf("\n");
+        printf("\nArrow Keys - NAVIGATE");
+        printf("\nEnter - END TURN");
         
-        // Game continues
-        if (turnDone) {
-            turnDone = false;
+        printf("\e[?25l"); // Hide the actual terminal cursor.
+        
+        if (cursorHidden) {
+            cursorHidden = false; // Unhide the game cursor.
         }
 
         // Keyhandler
@@ -224,48 +253,36 @@ int game()
 
         // The most repetitive code you'll ever see
         switch(ch) {
-            case 'A':
-                if (cursorY == 0) {
-                    cursorY = BOARD_Y_SIZE - 1;
-                }
-                else {
-                    cursorY--;
-                }
+            // If on the edges, loop back to the other side of the board.
+            case 'A': // Up
+                if (cursorY == 0) cursorY = BOARD_SIZE - 1;
+                else cursorY--;
                 break;
-            case 'B':
-                if (cursorY == BOARD_Y_SIZE - 1) {
-                    cursorY = 0;
-                }
-                else {
-                    cursorY++;
-                }
+            case 'B': // Down
+                if (cursorY == BOARD_SIZE - 1) cursorY = 0;
+                else cursorY++;
                 break;
-            case 'C':
-                if (cursorX == BOARD_X_SIZE - 1) {
-                    cursorX = 0;
-                }
-                else {
-                    cursorX++;
-                }
+            case 'C': // Right
+                if (cursorX == BOARD_SIZE - 1) cursorX = 0;
+                else cursorX++;
                 break;
-            case 'D':
-                if (cursorX == 0) {
-                    cursorX = BOARD_X_SIZE - 1;
-                }
-                else {
-                    cursorX--;
-                }
+            case 'D': // Left
+                if (cursorX == 0) cursorX = BOARD_SIZE - 1;
+                else cursorX--;
                 break;
+            // Listen for return carriage, when ENTER is pressed.
             case '\n':
-                if (board[cursorX][cursorY] != '.') {
+                if (getGridItem(board, cursorX, cursorY) != '.') {
                     illegalMove = true;
                 }
                 else {
-                    board[cursorX][cursorY] = currentPlayer;
+                    setGridItem(board, cursorX, cursorY, currentPlayer);
 
-                    turnDone = true;
+                    // Hide the cursor to reflect board change.
+                    cursorHidden = true;
+                    
+                    // Update game state.
                     turnCount++;
-
                     lastPlayer = currentPlayer;
 
                     if (currentPlayer == PLAYER_1_CHARACTER) {
@@ -276,12 +293,19 @@ int game()
                     }
                 }
                 break;
+            default: 
+                ; // Do nothing, if not any of arrow keys or ENTER.
         }
-	}
+    }
+    
+    // Free memory that was dynamically allocated for the board.
+    free(board);
+
+    // Kill game loop.
 	return 0;
 }
 
-// To allow game to loop
+// Starts program, loops the game when it ends.
 int main() {
     while(1) {
         game();
